52d054a0d276d0ef23a5c7fa682f2726fb02f668
Alexey Grigorev <alexey.s.grigoriev@gmail.com>
2026-05-23 16:49:37 +0200

    Prepare 0.2.2 release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,8 +46,8 @@ android {
         applicationId = "com.pocketshell.app"
         minSdk = 26
         targetSdk = 35
-        versionCode = 3
-        versionName = "0.2.1"
+        versionCode = 4
+        versionName = "0.2.2"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 

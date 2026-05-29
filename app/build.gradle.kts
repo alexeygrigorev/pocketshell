b8255a52d2977b13290aad8acfdbd3ee65f454dd
Alexey Grigorev <alexey.s.grigoriev@gmail.com>
2026-05-29 11:03:20 +0200

    Require configured PyPI token for publish

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,8 +46,8 @@ android {
         applicationId = "com.pocketshell.app"
         minSdk = 26
         targetSdk = 35
-        versionCode = 16
-        versionName = "0.3.4"
+        versionCode = 17
+        versionName = "0.3.5"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 

863c4bc88a8b3cebdf5d2334bd9fc7cc6c310338
Alexey Grigorev <alexey.s.grigoriev@gmail.com>
2026-05-31 22:50:20 +0200

    Bump to 0.3.12

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,8 +46,8 @@ android {
         applicationId = "com.pocketshell.app"
         minSdk = 26
         targetSdk = 35
-        versionCode = 23
-        versionName = "0.3.11"
+        versionCode = 24
+        versionName = "0.3.12"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 

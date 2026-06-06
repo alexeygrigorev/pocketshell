54b5d0d4a18049bcc06d3c372c79c78d1d0ee36e
Alexey Grigorev <alexey.s.grigoriev@gmail.com>
2026-06-06 13:31:58 +0200

    Bump version to 0.3.26

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,8 +46,8 @@ android {
         applicationId = "com.pocketshell.app"
         minSdk = 26
         targetSdk = 35
-        versionCode = 37
-        versionName = "0.3.25"
+        versionCode = 38
+        versionName = "0.3.26"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
